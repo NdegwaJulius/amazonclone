7162e33afc7c4bf5afaecb81772bba06573b8f53
Julius Ndegwa <juliusndegwa2@gmail.com>
2022-07-09 11:01:06 +0300

    first commit

---
+++ lib/features/auth/screens/auth_screen.dart
@@ -0,0 +1,57 @@
+import 'package:amazonclone/constants/global_variables.dart';
+import 'package:flutter/material.dart';
+
+enum Auth {
+  signin,
+  signup,
+}
+
+class AuthScreen extends StatefulWidget {
+  static const String routeName = '/auth_screen/';
+  const AuthScreen({Key? key}) : super(key: key);
+
+  @override
+  _AuthScreenState createState() => _AuthScreenState();
+}
+
+class _AuthScreenState extends State<AuthScreen> {
+  Auth _auth = Auth.signup;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: GlobalVariables.greyBackgroundCOlor,
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: SafeArea(
+          child:Column(
+            children: const [
+              Text ('Welcome', style: TextStyle(
+                fontSize: 22,
+                fontWeight: FontWeight.w500,
+              ),
+              ),
+              ListTile(
+                title: Text('Create Account',
+                  style:TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ) ,
+                ),
+                leading: Radio(
+                  activeColor: GlobalVariables.secondaryColor,
+                value:Auth.signup,
+                groupValue: _auth,
+                  onChanged: (Auth? val){
+                    setState(() {
+                      _auth = val!;
+                    });
+                  },
+                ),
+              ),
+            ],
+          ) ,
+        ),
+      ),
+    );
+  }
+}
