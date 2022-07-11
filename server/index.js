9c549b734374d01194ad251715c123ba6798ee6a
NdegwaJulius <juliusndegwa2@gmail.com>
2022-07-11 14:04:52 +0300

    Setting APIs

--- server/index.js
+++ server/index.js
@@ -3,12 +3,16 @@ const express = require('express');
 const mongoose = require('mongoose');
 //IMPORT FROM OTHER FILES
 const authRouter = require('./routes/auth');
+//const userRouter = require("./routes/user");
 //init
 const PORT = process.env.PORT || 3000;
 const app = express();
-const DB ="mongodb+srv://jul:julius@cluster0.s3bet.mongodb.net/?retryWrites=true&w=majority";
+const DB =
+"mongodb+srv://<jul>:<julius>@cluster0.s3bet.mongodb.net/?retryWrites=true&w=majority";
 //middleware
 app.use(authRouter);
+app.use(express.json());
+//app.use(userRouter);
 //clientside -> server -> client
 //connections
 mongoose
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,8 +1,36 @@
 const express = require('express');
-
+const User = require('../models/user');
+//const bcryptjs = require("bcryptjs");
 const authRouter = express.Router();
-authRouter.get('/user', (req, res)=>{
-    res.json({msg :"NdegwaJM"});
+
+authRouter.post('/api/signup', async  (req, res) => {
+  //get data from client
+   
+    try {
+        const {name,email,password} = req.body;
+        const existingUser = User.findOne({email});
+        if (existingUser) {
+         return res.status(400).json({msg:"User with the same email already exists"});
+         
+        }
+       // const hashedPassword = await bcryptjs.hash(password, 8);
+        
+        let user = new User({
+         email,
+         password,//:hashedPassword,
+         name,
+     
+        });
+        user = await user.save();
+        res.json(user);
+    } catch (e) {
+        res.status(500).json({Error:e.message});
+    }
+    
+  
+    //post the data to db
+
+    //return the data to the user
 
 });
 
